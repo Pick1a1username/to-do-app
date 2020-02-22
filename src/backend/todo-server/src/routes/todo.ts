eeb8f7238c0e792a2e0ac044f1925615ca8ab00e
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-22 16:52:02 +0900

    [Backend] todo.ts in routes linted.

--- src/backend/todo-server/src/routes/todo.ts
+++ src/backend/todo-server/src/routes/todo.ts
@@ -4,24 +4,24 @@ import * as todo from "../modules/todo";
 
 const router = express.Router();
 
-router.get("/", (request, response, next) => {
+router.get("/", (request, response) => {
   todo.findAllItems(response);
 });
 
-router.get("/:itemId", (request, response, next) => {
+router.get("/:itemId", (request, response) => {
   console.log(request.url + " : querying for " + request.params.itemId);
   todo.findItemById(request.params.itemId, response);
 });
 
-router.post("/", (request, response, next) => {
+router.post("/", (request, response) => {
   todo.saveItem(request, response);
 });
 
-router.put("/", (request, response, next) => {
+router.put("/", (request, response) => {
   todo.saveItem(request, response);
 });
 
-router.delete("/:itemId", (request, response, next) => {
+router.delete("/:itemId", (request, response) => {
   todo.remove(request, response);
 });
 
