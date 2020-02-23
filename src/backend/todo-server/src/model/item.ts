357894b036e8eb3e5503c0c757ae9b673da2ba4c
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-23 16:20:29 +0900

    [Backend] typedoc initialized.

--- src/backend/todo-server/src/model/item.ts
+++ src/backend/todo-server/src/model/item.ts
@@ -4,13 +4,20 @@ const Schema = mongoose.Schema;
 
 mongoose.connect("mongodb://todo_admin:some_password@mongo/todo");
 
-// https://medium.com/@tomanagle/strongly-typed-models-with-mongoose-and-typescript-7bc2f7197722
+/**
+ * Interface for something...
+ * 
+ * Reference: https://medium.com/@tomanagle/strongly-typed-models-with-mongoose-and-typescript-7bc2f7197722
+ */
 export interface ItemDocument extends mongoose.Document {
   itemId: string;
   text: string;
   completed: boolean;
 }
 
+/**
+ * Item's schema for Mongo DB
+ */
 const itemSchema = new Schema({
   itemId: { type: String, index: { unique: true } },
   text: String,
