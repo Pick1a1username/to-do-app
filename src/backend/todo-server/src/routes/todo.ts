1f58348ae357a6fe7f799aa8b168d312866e6fbe
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-23 16:56:35 +0900

    TypeDoc for Backend is set up.
    * [Backend] typedoc initialized.
    
    * [Backend] todo.ts in modules documented.
    
    * [Backend] todo.ts in routes commented.
    
    * [Backend] eslint check has been passed.

--- src/backend/todo-server/src/routes/todo.ts
+++ src/backend/todo-server/src/routes/todo.ts
@@ -4,23 +4,28 @@ import * as todo from "../modules/todo";
 
 const router = express.Router();
 
+// Find all items.
 router.get("/", (request, response) => {
   todo.findAllItems(response);
 });
 
+// Find an item by ID.
 router.get("/:itemId", (request, response) => {
   console.log(request.url + " : querying for " + request.params.itemId);
   todo.findItemById(request.params.itemId, response);
 });
 
+// Save or update an item.
 router.post("/", (request, response) => {
   todo.saveItem(request, response);
 });
 
+// Save or update an item.
 router.put("/", (request, response) => {
   todo.saveItem(request, response);
 });
 
+// Remove an item.
 router.delete("/:itemId", (request, response) => {
   todo.remove(request, response);
 });
